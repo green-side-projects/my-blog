6d5af223a9a696d0f4079fb14bdb5208e31d8479
asher01000001@gmail.com <asher01000001@gmail.com>
2022-12-26 08:42:35 +0800

    fix

--- lib/helper.tsx
+++ lib/helper.tsx
@@ -2,12 +2,12 @@
 //endpoint: http://localhost:3000/api/posts
 
 const baseUrl = "http://localhost:3000/api/posts";
-export default async function getPost(id) {
+export default async function getPost(id: number) {
   const res = await fetch(`${baseUrl}`);
   const posts = await res.json();
 
   if (id) {
-    return posts.find((value) => value.id == Number(id));
+    return posts.find((value: { id: number }) => value.id == Number(id));
   }
   return posts;
 }
